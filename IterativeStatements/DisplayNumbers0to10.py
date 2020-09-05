e193707095eeaaba38b2879982e0b51817d14dc1
Manoj's <ItsMe@MacBook-Pro.fios-router.home>
2020-09-05 15:44:09 -0400

    added examples for range function and for loop

---
+++ IterativeStatements/DisplayNumbers0to10.py
@@ -0,0 +1,13 @@
+for i in range(0,11):
+    print(i)
+
+
+#Display odd numbers from 0 to 20
+for i in range(0,21):
+    if i%2 == 1:
+        print(i)
+
+#Display numbers 10 to 1 (Descending order)
+
+for i in range(10,0,-1):
+    print(i)
