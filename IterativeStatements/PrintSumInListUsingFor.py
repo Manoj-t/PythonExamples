53b5d942cc307debb5afc0f69173693b2b989ac0
Manoj's <ItsMe@MacBook-Pro.fios-router.home>
2020-09-05 16:24:21 -0400

    added examples for while loop

---
+++ IterativeStatements/PrintSumInListUsingFor.py
@@ -0,0 +1,8 @@
+list = eval(input("Enter a list:"))
+
+sum = 0
+
+for x in list:
+    sum = sum + x;
+print("sum of elements in list:",sum)
+
---
+++ IterativeStatements/While loop/SumOfFirstnNumbers.py
@@ -0,0 +1,8 @@
+n = input("Enter a number:")
+sum = 0;
+i = 1;
+
+while i<=int(n):
+    sum = sum + i;
+    i = i+1;
+print("Sum of",n,"numbers is:",sum)
